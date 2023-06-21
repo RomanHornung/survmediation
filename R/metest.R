35c0159b0f224173cc5e19740901891800a90433
RomanHornung <hornung@ibe.med.uni-muenchen.de>
2023-06-21 10:54:55 +0200

    Added option to return Aalen models and mediator models.

--- R/metest.R
+++ R/metest.R
@@ -1,23 +1,49 @@
 metest <-
-function(times, cens, expos, M, z=NULL, simul=TRUE, npvalsim=1e+5, riskincr, exposinflc=TRUE, minflc=TRUE) {
-
-  if(class(M)=="numeric") {
-    metesttemp <- metestone(times=times, cens=cens, expos=expos, m=M, z=z, simul=simul, npvalsim=npvalsim, 
-                            riskincr=riskincr, exposinflc=exposinflc, minflc=minflc)
-    pvalues <- metesttemp$pvalue
-  }
-  
-  if(class(M)=="matrix") {
-    pvalues <- 0
-    for(i in 1:ncol(M)) { 
-      metesttemp <- metestone(times=times, cens=cens, expos=expos, m=M[,i], z=z, simul=simul, npvalsim=npvalsim, 
-                              riskincr=riskincr, exposinflc=exposinflc, minflc=minflc)
-      pvalues[i] <- metesttemp$pvalue
+  function(times, cens, expos, M, z=NULL, simul=TRUE, npvalsim=1e+5, riskincr, exposinflc=TRUE, minflc=TRUE, returnmodels=FALSE) {
+    
+    if(!returnmodels) {
+      
+      if("numeric" %in% class(M)) {
+        metesttemp <- metestone(times=times, cens=cens, expos=expos, m=M, z=z, simul=simul, npvalsim=npvalsim, 
+                                riskincr=riskincr, exposinflc=exposinflc, minflc=minflc)
+        pvalues <- metesttemp$pvalue
+      }
+      
+      if("matrix" %in% class(M)) {
+        pvalues <- 0
+        for(i in 1:ncol(M)) { 
+          metesttemp <- metestone(times=times, cens=cens, expos=expos, m=M[,i], z=z, simul=simul, npvalsim=npvalsim, 
+                                  riskincr=riskincr, exposinflc=exposinflc, minflc=minflc)
+          pvalues[i] <- metesttemp$pvalue
+        }
+      }
+      
+      params <- NULL
+      
+    } else {
+      
+      if("numeric" %in% class(M)) {
+        metesttemp <- metestone(times=times, cens=cens, expos=expos, m=M, z=z, simul=simul, npvalsim=npvalsim, 
+                                riskincr=riskincr, exposinflc=exposinflc, minflc=minflc)
+        pvalues <- metesttemp$pvalue
+        params <- metesttemp$params
+      }
+      
+      if("matrix" %in% class(M)) {
+        pvalues <- 0
+        params <- list()
+        for(i in 1:ncol(M)) { 
+          metesttemp <- metestone(times=times, cens=cens, expos=expos, m=M[,i], z=z, simul=simul, npvalsim=npvalsim, 
+                                  riskincr=riskincr, exposinflc=exposinflc, minflc=minflc)
+          pvalues[i] <- metesttemp$pvalue
+          params[[i]] <- metesttemp$params
+        }
+      }
+      
     }
+    
+    class(metest) <- "metest"
+    
+    return(list(pvalues=pvalues, params=params))
+    
   }
-  
-  class(metest) <- "metest"
-
-  return(list(pvalues=pvalues))
- 
-}
